d5586fd4b57fa36d76981a5b869c120b089afa98
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-10 16:48:05 -0700

    Update QuestionCard.tsx

--- react-quiz/src/components/QuestionCard.tsx
+++ react-quiz/src/components/QuestionCard.tsx
@@ -2,12 +2,35 @@ import React from "react";
 
 type Props = {
   question: string;
-  answer: string[];
+  answers: string[];
   callback: any;
-  userAnswer: string;
+  userAnswer: boolean;
   questionNr: number;
   totalQuestions: number;
 };
 
-const QuestionCard = () => <div>Question Card</div>;
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr}/{totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 export default QuestionCard;
