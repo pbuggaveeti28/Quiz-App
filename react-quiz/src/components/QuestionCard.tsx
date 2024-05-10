a3d3d0bb2de6443966590dc43de4f35a671894ed
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-10 16:27:45 -0700

    Added props to QuestionCard.tsx

--- react-quiz/src/components/QuestionCard.tsx
+++ react-quiz/src/components/QuestionCard.tsx
@@ -1,4 +1,13 @@
 import React from "react";
 
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNr: number;
+  totalQuestions: number;
+};
+
 const QuestionCard = () => <div>Question Card</div>;
 export default QuestionCard;
