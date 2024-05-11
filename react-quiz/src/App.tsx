c5e1f1e9696112eeba8a40779ca152ca3e122cf4
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-11 11:36:08 -0700

    Added difficulty

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import { fetchQuizQuestions } from "./API";
+
 //components
 import QuestionCard from "./components/QuestionCard";
+
+//types
+import { Difficulty } from "./API";
 const TOTAL_QUESTIONS = 10;
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -9,6 +14,8 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -21,14 +28,14 @@ const App = () => {
       </button>
       <p className="score">Score: </p>
       <p>Loading Questions...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
