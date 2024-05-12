e01eb1a8c8eb4bb199bb8b6c943c226abdd13e09
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-12 16:02:50 -0700

    Update App.tsx

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -41,7 +41,12 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      //users answer
+      const answer = e.currentTarget.value;
+    }
+  };
 
   const nextQuestion = () => {};
   return (
