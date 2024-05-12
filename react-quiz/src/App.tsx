db89e92b6b91c9749571f06a6cb0ee870c2538f0
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-12 16:08:19 -0700

    Checking correct answers

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -45,6 +45,19 @@ const App = () => {
     if (!gameOver) {
       //users answer
       const answer = e.currentTarget.value;
+
+      //check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      //add score if answer is correct
+      if (correct) setScore((prev) => prev + 1);
+      //save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
     }
   };
 
