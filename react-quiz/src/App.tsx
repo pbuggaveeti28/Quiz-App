1074d19f2cbc8adf6ac6f9f4af1c5552df481f70
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-05-12 16:15:27 -0700

    Update App.tsx

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -61,7 +61,16 @@ const App = () => {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    //move on to next question if not the last question
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
